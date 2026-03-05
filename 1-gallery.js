3b1944a60d602f27c05a5bbf04f54261100050ed
shovgen <shov.guitar@gmail.com>
2026-03-05 22:00:09 +0000

    Deploying to gh-pages from @ shovgen/goit-js-hw-09@7c0a1c3d862c5245c5ddb5e87739c2586664154c 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,2 @@
+import"./assets/styles-DD3qoKza.js";console.log("Gallary");
+//# sourceMappingURL=1-gallery.js.map
---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/styles-DD3qoKza.js";console.log("Form");
+//# sourceMappingURL=2-form.js.map
