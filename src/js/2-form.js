7828013c3dc95a1e65df8824303148485be76e15
Hanna Ilkiv <shov.guitar@gmail.com>
2026-03-10 19:36:01 +0100

    ubdate js-HW-09

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,48 @@
-console.log('Form')
+let formData = {
+    email: "",
+    message: ""
+};
+
+const form = document.querySelector('.feedback-form');
+
+const STORAGE_KEY = "feedback-form-state";
+
+
+const cheakForm = () => {
+    const saveData = localStorage.getItem(STORAGE_KEY);
+    if (saveData) {
+        const parsetData = JSON.parse(saveData);
+        formData = parsetData;
+        Object.entries(formData).forEach(([key, value]) => {
+            if (form.elements[key]) {
+                form.elements[key].value = value;
+            }
+        });
+    }
+};
+cheakForm();
+
+form.addEventListener('input', (e) => {
+    const { name, value } = e.target;
+    formData[name] = value.trim();
+
+
+    localStorage.setItem(STORAGE_KEY).
+        JSON.stringify(formData);
+});
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (formData.email == "" || formData.message === "") {
+        alert("fill please all fields");
+        return;
+    }
+
+    console.log("Submitted Data:", formData);
+    localStorage.removeItem(STORAGE_KEY);
+    formData = {
+        email: "", message: ""
+    };
+    form.reset();
+})
